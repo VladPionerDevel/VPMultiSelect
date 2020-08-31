d25deb71ac59f12b6206ca34c9c3892f25d39f80
VladPionerDevel <vladpionerdevel@gmail.com>
2020-08-31 16:45:44 +0300

    Initial commit

---
+++ Example/VPMultiSelect/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VPMultiSelect
+//
+//  Created by pionerdevel@gmail.com on 08/31/2020.
+//  Copyright (c) 2020 pionerdevel@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
